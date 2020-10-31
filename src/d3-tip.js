da0bcced75f56fa03ae9522233171150b6d031b8
bumbeishvili <me@davidb.dev>
2020-10-31 15:40:12 +0400

    Allow tree shaking

--- src/d3-tip.js
+++ src/d3-tip.js
@@ -2,13 +2,16 @@
 // Copyright (c) 2013 Justin Palmer
 // ES6 / D3 v4 Adaption Copyright (c) 2016 Constantin Gavrilete
 // Removal of ES6 for D3 v4 Adaption Copyright (c) 2016 David Gotz
-// Adaptation for d3.v6 by bumbeishvili (github.com/bumbeishvili)
-// NPM Publishing by bumbeishvili
+// Adaptation for d3.v6  and publish on NPM by bumbeishvili (github.com/bumbeishvili) - npm: d3-v6-tip
+//
 
 // Tooltips for d3.js SVG visualizations
-
-
 // import * as d3 from d3;
+
+import {select,selection} from "d3-selection";
+
+
+console.log(select)
 
 export default function () {
     const functor = function functor(v) {
@@ -93,7 +96,7 @@ export default function () {
                 getNodeEl()
                     .classed(v, true);
             } else {
-                d3.selection.prototype.attr.apply(getNodeEl(), args)
+                selection.prototype.attr.apply(getNodeEl(), args)
             }
 
         }
@@ -115,7 +118,7 @@ export default function () {
             if (args.length === 1) {
                 var styles = args[0];
                 Object.keys(styles).forEach(function (key) {
-                    return d3.selection.prototype.style.apply(getNodeEl(), [key, styles[key]]);
+                    return selection.prototype.style.apply(getNodeEl(), [key, styles[key]]);
                 });
             }
         }
@@ -254,9 +257,9 @@ export default function () {
 
     function initNode() {
         let tipNode2 = 2;
-        let tipNode = d3.select('.d3-tip-lib-node');
+        let tipNode = select('.d3-tip-lib-node');
         if (!tipNode.node()) {
-            tipNode = d3.select(document.createElement('div'))
+            tipNode = select(document.createElement('div'))
         }
         tipNode
             .classed('d3-tip-lib-node', true)
@@ -283,7 +286,7 @@ export default function () {
             // re-add node to DOM
             document.body.appendChild(node);
         };
-        return d3.select(node);
+        return select(node);
     }
 
     // Private - gets the screen coordinates of a shape
